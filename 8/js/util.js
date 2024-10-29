1877a13e168a38fa0357357815f7739f9b9427e3
Keks <keks@htmlacademy.ru>
2024-10-29 16:01:04 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/util.js
@@ -0,0 +1,24 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
+const getUniqueRandomInteger = (a, b, valuesArray) => {
+  let randomNumber = getRandomInteger(a, b);
+  while (valuesArray.includes(randomNumber)) {
+    randomNumber = getRandomInteger(a, b);
+    if (valuesArray.length >= (b - a + 1)) {
+      throw new Error(`Уникальных значиний в диапазоне от ${a} до ${b} больше нет!`);
+    }
+  }
+  valuesArray.push(randomNumber);
+  return randomNumber;
+};
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export { getRandomInteger, getRandomArrayElement, getUniqueRandomInteger, isEscapeKey };
