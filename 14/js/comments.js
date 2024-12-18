3f6d91bed4481332afb6f1277dfaf179a9bf71e0
Keks <keks@htmlacademy.ru>
2024-12-18 09:02:21 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/comments.js
@@ -0,0 +1,55 @@
+const COMMENTS_LOAD_STEP = 5;
+const bigPicture = document.querySelector('.big-picture');
+const commentsList = bigPicture.querySelector('.social__comments');
+const commentTemplate = commentsList.querySelector('.social__comment');
+const loadCommentsButton = bigPicture.querySelector('.comments-loader');
+const сommentsCountData = bigPicture.querySelector('.social__comment-count');
+
+let displayedCommentsCount = COMMENTS_LOAD_STEP;
+let allComments = [];
+
+const replaceAllComments = (newComments = []) => {
+  allComments = newComments;
+};
+
+const updateDisplayedCommentsCount = (newCount) => {
+  displayedCommentsCount = newCount;
+};
+
+const createCommentElement = (comment) => {
+  const commentElement = commentTemplate.cloneNode(true);
+  commentElement.querySelector('.social__picture').src = comment.avatar;
+  commentElement.querySelector('.social__picture').alt = comment.name;
+  commentElement.querySelector('.social__text').textContent = comment.message;
+  return commentElement;
+};
+
+const updateCommentsCountData = () => {
+  сommentsCountData.innerHTML = `${displayedCommentsCount} из <span class="comments-count">${allComments.length}</span> комментариев`;
+};
+
+const renderComments = () => {
+  commentsList.innerHTML = '';
+  const commentsFragment = document.createDocumentFragment();
+  displayedCommentsCount = Math.min(displayedCommentsCount, allComments.length);
+  const commentsToDisplay = allComments.slice(0, displayedCommentsCount);
+  updateCommentsCountData();
+
+  if (allComments.length <= COMMENTS_LOAD_STEP || displayedCommentsCount >= allComments.length) {
+    loadCommentsButton.classList.add('hidden');
+  } else {
+    loadCommentsButton.classList.remove('hidden');
+  }
+
+  commentsToDisplay.forEach((comment) => {
+    commentsFragment.appendChild(createCommentElement(comment));
+  });
+  commentsList.appendChild(commentsFragment);
+};
+
+const onLoadCommentsButtonClick = () => {
+  displayedCommentsCount += COMMENTS_LOAD_STEP;
+  renderComments();
+};
+
+export { replaceAllComments, updateDisplayedCommentsCount, renderComments, onLoadCommentsButtonClick };
---
+++ 14/js/main.js
@@ -0,0 +1,6 @@
+import { fetchPictures } from './pictures.js';
+import './form-handler.js';
+import './image-scale.js';
+import './effects-slider.js';
+
+fetchPictures();
